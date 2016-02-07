cf7353f6fdc65946b96c65625f2a237a03d7dbb1
Maksim S Makarov <m.s.makarov@gmail.com>
2016-02-07 16:00:00 +0300

    r

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,8 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-       #user ||= User.new # guest user (not logged in)
-       #user.role.name == "Admin"
+       user ||= User.new # guest user (not logged in)
+       user.role.name == "Admin"
         if user.admin?
           can :manage, :all
         elsif user.manager?

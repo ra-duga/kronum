592c9ceddaadb1af00ba69697cd492395b623258
Maksim S Makarov <m.s.makarov@gmail.com>
2016-02-07 13:14:07 +0300

    e

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'sprockets/railtie'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,9 +21,6 @@ module Kronum
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.enabled = true
 
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.assets.digest = false
 end
 end
